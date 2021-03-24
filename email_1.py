ae1c0fbe92157444607cbb3da295dff17452cbb8
Madi biradar <76739885+Madival-biradar@users.noreply.github.com>
2021-03-24 17:34:01 +0530

    Update email_1.py

--- email_1.py
+++ email_1.py
@@ -6,15 +6,15 @@ mail=Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'madivalbiradar14@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Madival.14@'
+app.config['MAIL_USERNAME'] = 'youremail@gmail.com'
+app.config['MAIL_PASSWORD'] = 'your password'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 @app.route("/")
 def index():
-   msg = Message('Hello', sender = 'madivalbiradar14@gmail.com', recipients = ['chandanchandy1998@gmail.com'])
+   msg = Message('Hello', sender = 'youremail@gmail.com', recipients = ['recepientsemail@gmail.com'])
    msg.body = "Hello Flask message sent from Flask-Mail  boss when we get holiday........."
    mail.send(msg)
    return "Sent"
